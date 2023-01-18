9c306609d1634638b6a580c157b8b55df0ea687a
Jaime Sánchez <jaimesanchez.93@gmail.com>
2023-01-18 10:56:01 +0000

    Update 3 files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import App from "./App";
 
-describe("wadus", () => {
+describe("App", () => {
   it("should render", () => {
     const renderInstance = render(<App />);
     expect(renderInstance.asFragment().firstChild).toBeDefined();
